b08aa30bda1a0f4473e5e41ce3e5a99e50352884
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2021-08-16 04:51:12 +0000

    responsive

--- src/App.js
+++ src/App.js
@@ -94,114 +94,115 @@ class App extends Component {
     const { formErrors } = this.state;
 
     return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>New Tractor Form</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="Brand">
-              <label htmlFor="Brand">Brand</label>
-              <input
-                className={formErrors.Brand.length > 0 ? "error" : null}
-                placeholder="Brand"
-                type="text"
-                name="Brand"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.Brand.length > 0 && (
-                <span className="errorMessage">{formErrors.Brand}</span>
-              )}
-            </div>
-            <div className="Model">
-              <label htmlFor="Model">Model</label>
-              <input
-                className={formErrors.Model.length > 0 ? "error" : null}
-                placeholder="Model"
-                type="text"
-                name="Model"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.Model.length > 0 && (
-                <span className="errorMessage">{formErrors.Model}</span>
-              )}
-            </div>
-            <div className="Variant">
-              <label htmlFor="Variant">Variant</label>
-              <input
-                className={formErrors.Variant.length > 0 ? "error" : null}
-                placeholder="Variant"
-                type="text"
-                name="Variant"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.Variant.length > 0 && (
-                <span className="errorMessage">{formErrors.Variant}</span>
-              )}
-            </div>
-            <div className="Hp">
-              <label htmlFor="Hp">Hp</label>
-              <input
-                className={formErrors.Hp.length > 0 ? "error" : null}
-                placeholder="Hp"
-                type="text"
-                name="Hp"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.Hp.length > 0 && (
-                <span className="errorMessage">{formErrors.Hp}</span>
-              )}
-            </div>
-            <div className="Rpm">
-              <label htmlFor="Rpm">Rpm</label>
-              <input
-                className={formErrors.Rpm.length > 0 ? "error" : null}
-                placeholder="Rpm"
-                type="text"
-                name="Rpm"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.Rpm.length > 0 && (
-                <span className="errorMessage">{formErrors.Rpm}</span>
-              )}
-            </div>
-            <div className="VehicleOverview">
-              <label htmlFor="VehicleOverview">Vehicle-Overview</label>
-              <input
-                className={formErrors.VehicleOverview.length > 0 ? "error" : null}
-                placeholder="VehicleOverview"
-                type="text"
-                name="VehicleOverview"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.VehicleOverview.length > 0 && (
-                <span className="errorMessage">{formErrors.VehicleOverview}</span>
-              )}
-            </div>
-            <div className="VehicleFeatures">
-              <label htmlFor="VehicleFeatures">VehicleFeatures</label>
-              <input
-                className={formErrors.VehicleFeatures.length > 0 ? "error" : null}
-                placeholder="VehicleFeatures"
-                type="VehicleFeatures"
-                name="VehicleFeatures"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.VehicleFeatures.length > 0 && (
-                <span className="errorMessage">{formErrors.VehicleFeatures}</span>
-              )}
-            </div>
-            <form>
-                <div className="form-group multi-preview">
-                    {(this.fileArray || []).map(url => (
-                        <img src={url} alt="..." />
-                    ))}
-                </div>
+      <div class="container">
+        <div className="wrapper">
+          <div className="form-wrapper">
+            <h1>New Tractor Form</h1>
+            <form onSubmit={this.handleSubmit} noValidate>
+              <div className="Brand">
+                <label htmlFor="Brand">Brand</label>
+                <input
+                  className={formErrors.Brand.length > 0 ? "error" : null}
+                  placeholder="Brand"
+                  type="text"
+                  name="Brand"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.Brand.length > 0 && (
+                  <span className="errorMessage">{formErrors.Brand}</span>
+                )}
+              </div>
+              <div className="Model">
+                <label htmlFor="Model">Model</label>
+                <input
+                  className={formErrors.Model.length > 0 ? "error" : null}
+                  placeholder="Model"
+                  type="text"
+                  name="Model"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.Model.length > 0 && (
+                  <span className="errorMessage">{formErrors.Model}</span>
+                )}
+              </div>
+              <div className="Variant">
+                <label htmlFor="Variant">Variant</label>
+                <input
+                  className={formErrors.Variant.length > 0 ? "error" : null}
+                  placeholder="Variant"
+                  type="text"
+                  name="Variant"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.Variant.length > 0 && (
+                  <span className="errorMessage">{formErrors.Variant}</span>
+                )}
+              </div>
+              <div className="Hp">
+                <label htmlFor="Hp">Hp</label>
+                <input
+                  className={formErrors.Hp.length > 0 ? "error" : null}
+                  placeholder="Hp"
+                  type="text"
+                  name="Hp"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.Hp.length > 0 && (
+                  <span className="errorMessage">{formErrors.Hp}</span>
+                )}
+              </div>
+              <div className="Rpm">
+                <label htmlFor="Rpm">Rpm</label>
+                <input
+                  className={formErrors.Rpm.length > 0 ? "error" : null}
+                  placeholder="Rpm"
+                  type="text"
+                  name="Rpm"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.Rpm.length > 0 && (
+                  <span className="errorMessage">{formErrors.Rpm}</span>
+                )}
+              </div>
+              <div className="VehicleOverview">
+                <label htmlFor="VehicleOverview">Vehicle-Overview</label>
+                <input
+                  className={formErrors.VehicleOverview.length > 0 ? "error" : null}
+                  placeholder="VehicleOverview"
+                  type="text"
+                  name="VehicleOverview"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.VehicleOverview.length > 0 && (
+                  <span className="errorMessage">{formErrors.VehicleOverview}</span>
+                )}
+              </div>
+              <div className="VehicleFeatures">
+                <label htmlFor="VehicleFeatures">VehicleFeatures</label>
+                <input
+                  className={formErrors.VehicleFeatures.length > 0 ? "error" : null}
+                  placeholder="VehicleFeatures"
+                  type="VehicleFeatures"
+                  name="VehicleFeatures"
+                  noValidate
+                  onChange={this.handleChange}
+                />
+                {formErrors.VehicleFeatures.length > 0 && (
+                  <span className="errorMessage">{formErrors.VehicleFeatures}</span>
+                )}
+              </div>
+              <form>
+                  <div className="form-group multi-preview">
+                      {(this.fileArray || []).map(url => (
+                          <img src={url} alt="..." />
+                      ))}
+                  </div>
 
                 <div className="form-group">
                     <input type="file" className="form-control" onChange={this.uploadMultipleFiles} multiple />
@@ -214,6 +215,7 @@ class App extends Component {
             </div>
           </form>
         </div>
+      </div>
       </div>
     );
   }
